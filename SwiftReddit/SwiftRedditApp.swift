6408e023cd3a88ff78ab4a7f4c320cd9d4dfff9f
Zabir Raihan <zabirraihan@icloud.com>
2025-06-15 20:20:15 +0600

    Initial Commit

---
+++ SwiftReddit/SwiftRedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRedditApp.swift
+//  SwiftReddit
+//
+//  Created by Zabir Raihan on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
