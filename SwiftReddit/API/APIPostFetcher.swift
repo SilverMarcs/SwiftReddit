7cca8838e1838f7928947b3448a2d4abf1898ecb
Zabir Raihan <zabirraihan@icloud.com>
2025-06-27 01:25:40 +0600

    feat: implement image prefetching for posts in APIPostFetcher

--- SwiftReddit/API/APIPostFetcher.swift
+++ SwiftReddit/API/APIPostFetcher.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Kingfisher
 
 extension RedditAPI {
     func fetchPosts(for feedType: PostFeedType, sort: SubListingSortOption = .best, after: String? = nil, limit: Int = 10) async -> ([Post], String?)? {
@@ -75,6 +76,60 @@ extension RedditAPI {
             return Post(from: child.data)
         }
         
+        // Prefetch images for all posts
+        prefetchImagesForPosts(posts)
+        
         return (posts, listingResponse.data.after)
+    }
+    
+    private func prefetchImagesForPosts(_ posts: [Post]) {
+        Task.detached {
+            let imageURLs = await self.extractImageURLsFromPosts(posts)
+            let urls = imageURLs.compactMap { URL(string: $0) }
+            
+            if !urls.isEmpty {
+                let prefetcher = ImagePrefetcher(urls: urls)
+                prefetcher.start()
+            }
+        }
+    }
+    
+    private func extractImageURLsFromPosts(_ posts: [Post]) -> [String] {
+        var imageURLs: [String] = []
+        
+        for post in posts {
+            switch post.mediaType {
+            case .image(let galleryImage):
+                imageURLs.append(galleryImage.url)
+                
+            case .gallery(let images):
+                imageURLs.append(contentsOf: images.map { $0.url })
+                
+            case .video(_, let thumbnailURL, _):
+                if let thumbnailURL = thumbnailURL {
+                    imageURLs.append(thumbnailURL)
+                }
+                
+            case .youtube(_, let galleryImage):
+                imageURLs.append(galleryImage.url)
+                
+            case .gif(let galleryImage):
+                imageURLs.append(galleryImage.url)
+                
+            case .link(let metadata):
+                if let thumbnailURL = metadata.thumbnailURL {
+                    imageURLs.append(thumbnailURL)
+                }
+                
+            case .repost(let originalPost):
+                let nestedURLs = extractImageURLsFromPosts([originalPost])
+                imageURLs.append(contentsOf: nestedURLs)
+                
+            case .none:
+                break
+            }
+        }
+        
+        return imageURLs
     }
 }
