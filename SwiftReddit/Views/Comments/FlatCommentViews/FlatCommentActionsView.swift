1da1534f74de2332aaaa0f598115951cd3f4dea6
Zabir Raihan <zabirraihan@icloud.com>
2025-07-11 17:34:14 +0600

    refactor: simplify FlatCommentActionsView by integrating VoteActionViewModel and removing state variables

--- SwiftReddit/Views/Comments/FlatCommentViews/FlatCommentActionsView.swift
+++ SwiftReddit/Views/Comments/FlatCommentViews/FlatCommentActionsView.swift
@@ -9,52 +9,32 @@ import SwiftUI
 
 struct FlatCommentActionsView: View {
     let comment: FlatComment
-    @State private var likes: Bool?
-    @State private var upsCount: Int
+    private var viewModel: VoteActionViewModel
     
     init(comment: FlatComment) {
         self.comment = comment
-        self._likes = State(initialValue: comment.likes)
-        self._upsCount = State(initialValue: comment.ups)
+        viewModel = VoteActionViewModel(item: comment, targetType: .comment)
     }
     
     var body: some View {
         HStack(alignment: .center) {
-            Button(action: { 
-                // Handle upvote
-                if likes == true {
-                    likes = nil
-                    upsCount = comment.ups
-                } else {
-                    likes = true
-                    upsCount = comment.ups + (comment.likes == false ? 2 : 1)
-                }
-            }) {
+            Button(action: { viewModel.vote(action: .up) }) {
                 Image(systemName: "arrow.up")
                     .font(.subheadline)
-                    .foregroundStyle(likes == true ? .indigo : .secondary)
+                    .foregroundStyle(viewModel.likes == true ? .indigo : .secondary)
             }
             .buttonStyle(.plain)
             
-            Text(upsCount.formatted)
+            Text(viewModel.upsCount.formatted)
                 .contentTransition(.numericText())
                 .font(.subheadline)
                 .fontWeight(.semibold)
-                .foregroundStyle(likes == true ? .indigo : likes == false ? .red : .secondary)
+                .foregroundStyle(viewModel.likes == true ? .indigo : viewModel.likes == false ? .red : .secondary)
             
-            Button(action: { 
-                // Handle downvote
-                if likes == false {
-                    likes = nil
-                    upsCount = comment.ups
-                } else {
-                    likes = false
-                    upsCount = comment.ups - (comment.likes == true ? 2 : 1)
-                }
-            }) {
+            Button(action: { viewModel.vote(action: .down) }) {
                 Image(systemName: "arrow.down")
                     .font(.subheadline)
-                    .foregroundStyle(likes == false ? .red : .secondary)
+                    .foregroundStyle(viewModel.likes == false ? .red : .secondary)
             }
             .buttonStyle(.plain)
         }
