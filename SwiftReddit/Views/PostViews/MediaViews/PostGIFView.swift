68bab03f1dfba1a0e29efd3515f46dc5f1208a4e
Zabir Raihan <zabirraihan@icloud.com>
2025-07-12 11:46:17 +0600

    open gif in in app browser

--- SwiftReddit/Views/PostViews/MediaViews/PostGIFView.swift
+++ SwiftReddit/Views/PostViews/MediaViews/PostGIFView.swift
@@ -8,19 +8,17 @@
 import SwiftUI
 
 struct PostGIFView: View {
-    @Environment(Nav.self) private var nav
-  let galleryImage: GalleryImage
+    let galleryImage: GalleryImage
+    @Environment(\.openURL) var openURL
   
   var body: some View {
       Button {
-          let linkMetadata = LinkMetadata(
-            url: galleryImage.url,
-            domain: galleryImage.url,
-            thumbnailURL: nil
-          )
-          nav.path.append(linkMetadata)
+          if let url = URL(string: galleryImage.url) {
+              openURL(url, prefersInApp: true)
+          }
       } label: {
           PostImageView(image: galleryImage)
+              .disabled(true)
               .overlay(alignment: .bottomTrailing) {
                   Text("GIF")
                   .font(.caption2)
